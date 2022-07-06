c2951feb315e1c072c7a91877ea49e8b0355d044
Jakub Kwiatkowski <jakub.kwiatkowski@binarapps.com>
2022-07-06 10:09:22 +0200

    Initial commit

---
+++ src/test/java/com/jkwiatko/adminappbackend/AdminAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkwiatko.adminappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
