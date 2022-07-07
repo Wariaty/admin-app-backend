27c9ca8d4221252c22691b76e63bf0c1cb63c9b4
Jakub Kwiatkowski <jakub.kwiatkowski@binarapps.com>
2022-07-07 20:41:03 +0200

    Add user endpoints

---
+++ src/main/java/com/jkwiatko/adminappbackend/user/controller/UserController.java
@@ -0,0 +1,63 @@
+package com.jkwiatko.adminappbackend.user.controller;
+
+import com.jkwiatko.adminappbackend.user.db.entity.User;
+import com.jkwiatko.adminappbackend.user.db.repository.UserRepository;
+import com.jkwiatko.adminappbackend.user.model.request.UserRequest;
+import com.jkwiatko.adminappbackend.user.model.response.UserResponse;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private ModelMapper modelMapper;
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    @GetMapping
+    public List<UserResponse> login() {
+        return userRepository.findAll().stream().map(user -> modelMapper.map(user, UserResponse.class)).toList();
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody @Valid UserRequest userRequest) {
+        var user = new User();
+        user.setEmail(userRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(userRequest.getPassword()));
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@AuthenticationPrincipal UserDetails currentUser, @RequestBody @Valid UserRequest userRequest, @PathVariable Long id) {
+        if (isCurrentUserIdDiffer(currentUser, id)) {
+            var user = userRepository.getById(id);
+            user.setEmail(userRequest.getEmail());
+            user.setPassword(passwordEncoder.encode(userRequest.getPassword()));
+            return userRepository.save(user);
+        } else {
+            throw new IllegalArgumentException("You cannot edit currently logged in user");
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void removeUser(@AuthenticationPrincipal UserDetails currentUser, @PathVariable Long id) {
+        if (isCurrentUserIdDiffer(currentUser, id)) {
+            userRepository.deleteById(id);
+        } else {
+            throw new IllegalArgumentException("You cannot delete currently logged in user");
+        }
+    }
+
+    private boolean isCurrentUserIdDiffer(UserDetails currentUser, Long id) {
+        return userRepository.findByEmail(currentUser.getUsername()).map(User::getId).filter(id::equals).isEmpty();
+    }
+}
---
+++ src/main/java/com/jkwiatko/adminappbackend/user/model/response/UserResponse.java
@@ -0,0 +1,9 @@
+package com.jkwiatko.adminappbackend.user.model.response;
+
+import lombok.Data;
+
+@Data
+public class UserResponse {
+    private Long id;
+    private String email;
+}
